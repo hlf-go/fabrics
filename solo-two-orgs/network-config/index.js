6958ad23eaa9dec0d6b9fefac0535bac9d7884e0
Paul Sitoh <paulwizviz@gmail.com>
2018-03-02 15:25:11 +0000

    Update delivery of network configuration
    modified:   solo-two-orgs/network-config/index.js

--- solo-two-orgs/network-config/index.js
+++ solo-two-orgs/network-config/index.js
@@ -1,10 +1,9 @@
 'use strict'
 
 const express = require('express');
-const org1 = require('./org1.json')
-const org2 = require('./org2.json')
-
 const modifyConfig = require('./util.js').modifyConfig
+const org1Config = modifyConfig(require('./org1.json'))
+const org2Config = modifyConfig(require('./org2.json'))
 
 // Constants
 const PORT = 8080;
@@ -13,11 +12,11 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/org1', (req, res) => {
-  res.send(modifyConfig(org1));
+  res.send(org1Config);
 });
 
 app.get('/org2', (req, res) => {
-  res.send(modifyConfig(org2));
+  res.send(org2Config);
 });
 
 app.listen(PORT, HOST);
